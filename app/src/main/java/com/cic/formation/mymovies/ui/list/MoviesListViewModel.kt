e2ecc93902280d2edd36f4861f45bdfb39e7e9d3
Zakaria Boukaddouss <zakaria.boukaddouss@ibm.com>
2020-12-13 13:44:31 +0100

    [NO-TASK] REFACTOR - Add scopes to viewModels

--- app/src/main/java/com/cic/formation/mymovies/ui/list/MoviesListViewModel.kt
+++ app/src/main/java/com/cic/formation/mymovies/ui/list/MoviesListViewModel.kt
@@ -7,7 +7,9 @@ import androidx.lifecycle.asLiveData
 import com.cic.formation.mymovies.data.api.json.Movies
 import com.cic.formation.mymovies.data.utils.Results
 import com.cic.formation.mymovies.domain.GetMoviesUseCase
+import dagger.hilt.android.scopes.FragmentScoped
 
+@FragmentScoped
 class MoviesListViewModel @ViewModelInject constructor(
     getMoviesUseCase: GetMoviesUseCase
 ) : ViewModel() {
